679788097517396ebac49f44348ba42a64c42c58
Jaeiya <exanix@gmail.com>
2025-07-11 14:17:41 -0700

    feat(timer): add side-dependent timer activation
    We also use milliseconds to calculate ticks instead of iterations.

--- timer/timer.lua
+++ timer/timer.lua
@@ -21,7 +21,6 @@
 local speed        = 0.05 -- 50ms per tick (min is 0.05 because of rounding)
 local iterations   = 0
 local milliseconds = 0
-local redstoneDir  = "front"
 
 local modem = peripheral.find("modem")
 if not modem then
@@ -40,26 +39,20 @@ rednet.open(peripheral.getName(modem))
 
 
 local function getTimeStr()
-    local seconds     = math.floor(milliseconds / 1000)
-    local minutes     = math.floor(seconds / 60)
-    local hours       = math.floor(minutes / 60)
+    local ticks   = math.floor(milliseconds / 50)
+    local seconds = math.floor(milliseconds / 1000)
+    local minutes = math.floor(seconds / 60)
+    local hours   = math.floor(minutes / 60)
+
 
     local str = string.format(
         "%02d:%02d:%02d.%02d",
-        hours       % 60,
-        minutes     % 60,
-        seconds     % 60,
-        iterations  % 20
+        hours   % 60,
+        minutes % 60,
+        seconds % 60,
+        ticks   % 20
     )
     return str
-end
-
-
-local function writeFile(filepath, text)
-    local file = fs.open(filepath, "w")
-    file.write(text)
-    file.close()
-    return true
 end
 
 
@@ -75,21 +68,32 @@ local function startTimer()
             milliseconds = iterations * (speed * 1000)
             rednet.broadcast(getTimeStr(), protocol)
             timerID = os.startTimer(speed)
-        elseif event == "redstone" and redstone.getInput(redstoneDir) then
-            os.cancelTimer(timerID)
-            local timeStr = getTimeStr()
-            rednet.broadcast(timeStr, protocol)
-            -- writeFile("temp.txt", timeStr)
-            break
+
+        elseif event == "redstone" then
+            local right = redstone.getInput("right")
+            local left = redstone.getInput("left")
+
+            if right or left then
+                os.cancelTimer(timerID)
+
+                if right then
+                    milliseconds = 0
+                elseif left then
+                   -- send event to leaderboard
+                end
+
+                local timeStr = getTimeStr()
+                rednet.broadcast(timeStr, protocol)
+                break
+            end
         end
     end
 end
 
-print("AcceptsRedstone: "..redstoneDir)
 print(" BroadcastingOn: ".. protocol)
 while true do
     os.pullEvent("redstone")
-    if redstone.getInput(redstoneDir) then
+    if redstone.getInput("right") then
         startTimer()
     end
 end
