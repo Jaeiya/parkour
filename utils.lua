f8c4b2b34672a6e7c84b67118eb84911be1e4cd3
Jaeiya <exanix@gmail.com>
2025-07-12 23:30:40 -0700

    feat(utils): add more utility methods

--- utils.lua
+++ utils.lua
@@ -17,6 +17,64 @@ utils.writeFile = function(filepath, text)
     file.close()
 end
 
+utils.getPeripheral = function(name)
+    local p = peripheral.find(name)
+    if not p then
+        error("missing peripheral: " .. name)
+    end
+    return p
+end
+
+-- sum adds all variadic arguments together
+utils.sum = function(...)
+    local total = 0
+    for _, num in ipairs({...}) do
+        total = total + num
+    end
+    return total
+end
+
+-- diffCoords compares two coordinates and determines
+-- their absolute distance.
+local function diffCoords(coord1, coord2)
+    if coord1 == coord2 then
+        return 0
+    elseif coord1 <= 0 and coord2 > 0 then
+        return math.abs(coord1) + coord2
+    elseif coord1 >= 0 and coord2 < 0 then
+        return coord1 + math.abs(coord2)
+    else
+        -- Both coords are either positive or negative
+        return math.abs(math.abs(coord1) - math.abs(coord2))
+    end
+end
+
+
+utils.getNearestPlayer = function(x, y, z, playerDetector)
+    local players = playerDetector.getOnlinePlayers()
+    local nearestPlayer = ""
+    local nearestPos   = math.huge
+
+    for i = 1, #players do
+        local playerPosObj = playerDetector.getPlayerPos(players[i])
+        local playerPosDiff = utils.sum(
+            diffCoords(x, playerPosObj.x),
+            diffCoords(y, playerPosObj.y),
+            diffCoords(z, playerPosObj.z)
+        )
+        if playerPosDiff < nearestPos then
+            nearestPos = playerPosDiff
+            nearestPlayer = players[i]
+        end
+    end
+
+    return {
+        name = nearestPlayer,
+        distance = nearestPos
+    }
+end
+
+
 -- centerText on a specified monitor
 utils.centerText = function(text, mon)
     local w = mon.getSize()
