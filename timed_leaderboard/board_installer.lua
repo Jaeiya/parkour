4bdce5ddcca39ca54f520a338a0582d1b2d46796
Jaeiya <exanix@gmail.com>
2025-07-17 16:09:53 -0700

    fix(board_installer): remove test code

--- timed_leaderboard/board_installer.lua
+++ timed_leaderboard/board_installer.lua
@@ -14,6 +14,7 @@ local paths = {
     boardStartup   = "disk/board_startup",
     board          = "disk/board",
     boardData      = "disk/board_lib",
+    -- boardUI        = "disk/board_ui",
     boardRedstone  = "disk/board_redstone",
     utils          = "disk/utils",
     playerDetector = "disk/detect_player"
@@ -92,6 +93,10 @@ f.close()
 f = fs.open(paths.boardData, "r")
 utils.writeFile("/board_lib", f.readAll())
 f.close()
+
+-- f = fs.open(paths.boardUI, "r")
+-- utils.writeFile("/board_ui", f.readAll())
+-- f.close()
 
 f = fs.open(paths.boardStartup, "r")
 utils.writeFile("/startup", f.readAll())
