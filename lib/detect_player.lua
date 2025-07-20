4d0bfee298d7bd5887bd12f9108f3b03856bc804
Jaeiya <exanix@gmail.com>
2025-07-20 00:29:32 -0700

    feat(detect_player): re-write with detection events
    Apparently the advanced peripherals mod adds its own playerJoin and
    playerLeave events that I can listen for, so this significantly
    simplifies polling for player changes.

--- lib/detect_player.lua
+++ lib/detect_player.lua
@@ -1,43 +1,44 @@
 local utils = require("utils")
 
-local intervalDelay = 2 -- seconds
-
-local players = pd.getOnlinePlayers()
 local pd        = utils.getPlayerDetector()
 local players   = pd.getOnlinePlayers()
+local listeners = {}
+
+
+local function updateListeners()
+    for _, listener in ipairs(listeners) do
+        os.queueEvent(listener.event, {
+            action = listener.action,
+            payload = players,
+        })
+    end
+end
+
+
+
+local function startListening()
+    parallel.waitForAny(
+        function ()
+           while true do
+                os.pullEvent("playerJoin")
+                players = pd.getOnlinePlayers()
+                updateListeners()
+           end
+        end,
+        function ()
+           while true do
+                os.pullEvent("playerLeave")
+                players = pd.getOnlinePlayers()
+                updateListeners()
+           end
+        end
+    )
+end
 
 return function(...)
-    local listeners = {}
     for _, listener in ipairs({...}) do
         listeners[#listeners+1] = listener
         os.queueEvent(listener.event, {action = listener.action, payload = players})
     end
-
-    local function updateListeners()
-        for _, listener in ipairs(listeners) do
-            os.queueEvent(listener.event, {
-                action = listener.action,
-                payload = players,
-            })
-        end
-    end
-
-    while true do
-        sleep(intervalDelay)
-
-        local newPlayers = pd.getOnlinePlayers()
-        if #newPlayers ~= #players then
-            players = newPlayers
-            updateListeners()
-
-        else
-            for i, newPlayer in ipairs(newPlayers) do
-                if newPlayer ~= players[i] then
-                    players = newPlayers
-                    updateListeners()
-                    break
-                end
-            end
-        end
-    end
+    startListening()
 end
