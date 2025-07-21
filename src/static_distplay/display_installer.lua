9d3cfe0e4065e623d9a9e07222b1134813593710
Jaeiya <exanix@gmail.com>
2025-07-20 23:50:46 -0700

    feat(display_installer): add extensive help when missing peripherals

--- src/static_distplay/display_installer.lua
+++ src/static_distplay/display_installer.lua
@@ -1,6 +1,61 @@
 
 local utils = require("utils")
 
+
+print()
+local modem = utils.getModem()
+if not modem then
+    printError("Installation Aborted")
+    term.setTextColor(colors.orange)
+    write("Attach a ")
+    term.setTextColor(colors.lime)
+    write("wired modem ")
+    term.setTextColor(colors.orange)
+    write("to this computer and connect it (with ")
+    term.setTextColor(colors.lime)
+    write("networking cable")
+    term.setTextColor(colors.orange)
+    write(") to another modem, attached to an ")
+    term.setTextColor(colors.lime)
+    write("advanced monitor\n\n")
+    term.setTextColor(colors.orange)
+    write("Make sure you ")
+    term.setTextColor(colors.lime)
+    write("right click ")
+    term.setTextColor(colors.orange)
+    write("the modems to activate them, once you've finished connecting them.\n\n")
+    return
+end
+
+
+---@type Monitor|nil
+local mon = nil
+
+-- Find the monitor connected through the modem
+for _, name in ipairs(peripheral.getNames()) do
+    if peripheral.getType(name) == "monitor" then
+        mon = peripheral.wrap(name)
+        break
+    end
+end
+
+if not mon then
+    printError("Installation Aborted")
+    term.setTextColor(colors.orange)
+    write("Attach an ")
+    term.setTextColor(colors.lime)
+    write("advanced monitor ")
+    term.setTextColor(colors.orange)
+    write("to the modem on the other side of the networking cable.\n\n")
+    write("Make sure you ")
+    term.setTextColor(colors.lime)
+    write("right clicked ")
+    term.setTextColor(colors.orange)
+    write("the modem on the other side.\n\n")
+    return
+end
+
+
 ---@class DisplayPaths
 ---@field display string
 ---@field utils string
