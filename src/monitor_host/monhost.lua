204e5c2d42eb4154ec1af11bc466be96ce771930
Jaeiya <exanix@gmail.com>
2025-07-21 16:28:05 -0700

    feat(monhost): use `utils.print()`

--- src/monitor_host/monhost.lua
+++ src/monitor_host/monhost.lua
@@ -51,14 +51,10 @@ end
 
 if not isValidMonitorSize() then error("Must be 5 blocks long") end
 
-term.setTextColor(colors.lightGray)
-write("  host_name: ")
-term.setTextColor(colors.cyan)
-write(config.hostname .. "\n")
-term.setTextColor(colors.lightGray)
-write("   protocol: ")
-term.setTextColor(colors.cyan)
-write(config.protocol .. "\n")
+utils.print(
+    ";lgy;  host_name: ;cyn;"..config.hostname.."\n"..
+    ";lgy;   protocol: ;cyn;"..config.protocol
+)
 
 -- Default to zero values
 mon.setCursorPos(1, 1)
