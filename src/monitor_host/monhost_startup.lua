44500c602a2e4512c485eeb31be814d5e437062b
Jaeiya <exanix@gmail.com>
2025-07-20 21:31:31 -0700

    docs: add docs to startup files

--- src/monitor_host/monhost_startup.lua
+++ src/monitor_host/monhost_startup.lua
@@ -1,9 +1,25 @@
 
+
+---
+---
+---Starts all necessary scripts to run the monitor host.
+---
+---When installed, this file is renamed to 'startup', which causes
+---the computer to execute this script every time the computer is
+---turned on.
+---
+---The conditions for turning on the computer can either be through
+---player interaction or chunk loading. If the computer is left 'on'
+---when the world is closed, it will start 'on' when the chunk
+---is loaded again.
+---
+---
+
+
 term.clear()
 term.setCursorPos(1, 1)
 
 -- Executes the monitor host and any other scripts in parallel if needed
-term.clear()
 parallel.waitForAny(
     function ()
         print("")
--- src/timed_leaderboard/board_startup.lua
+++ src/timed_leaderboard/board_startup.lua
@@ -1,4 +1,21 @@
 
+
+---
+---
+---Starts all necessary scripts to run the leaderboard.
+---
+---When installed, this file is renamed to 'startup', which causes
+---the computer to execute this script every time the computer is
+---turned on.
+---
+---The conditions for turning on the computer can either be through
+---player interaction or chunk loading. If the computer is left 'on'
+---when the world is closed, it will start 'on' when the chunk
+---is loaded again.
+---
+---
+
+
 term.clear()
 term.setCursorPos(1, 1)
 
@@ -21,7 +38,7 @@ parallel.waitForAny(
     function() execRedstone() end,
     function() execPlayerDetection({
         event = "leaderboard",
-        action = "set_player_list"
+        action = "set_player_list",
     }) end,
     function() execBoardUI() end
 )
