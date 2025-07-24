d8fd53dc514e6272a4697ecd4ec28af5d88ce664
Jaeiya <exanix@gmail.com>
2025-07-24 06:53:14 -0700

    fix(get): bump patch verison & version should be a string
    Not sure why in the world I set version to a number lol

--- src/get.lua
+++ src/get.lua
@@ -19,7 +19,7 @@
 
 
 -- Should be kept up to date with latest tagged version of repository
-local version = 2.4
+local version = "2.3.1"
 
 
 ---Color code map designed strictly for use with utils.printColor()
