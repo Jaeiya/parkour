3768c7892ce5b68b6585973e62b039546879baaf
Jaeiya <exanix@gmail.com>
2025-07-25 00:52:33 -0700

    chore(get): bump version to 2.4.6

--- src/get.lua
+++ src/get.lua
@@ -19,7 +19,7 @@
 
 
 -- Should be kept up to date with latest tagged version of repository
-local version = "2.4.5"
+local version = "2.4.6"
 
 
 ---Color code map designed strictly for use with utils.printColor()
