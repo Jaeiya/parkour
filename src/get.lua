6d514b34afeb674773b6bf129c6a765b36b77423
Jaeiya <exanix@gmail.com>
2025-08-14 12:59:10 -0700

    chore(get): bump `copydisk` version to 2.1

--- src/get.lua
+++ src/get.lua
@@ -217,7 +217,7 @@ local diskMap = {
         }
     },
     copydisk = {
-        version = "2.0",
+        version = "2.1",
         name = "CopyDisk",
         scripts = {
             scriptMap.get,
