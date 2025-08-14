1ba27d6381368e11487e64b0df7d4ab1ab4abb04
Jaeiya <exanix@gmail.com>
2025-08-14 12:58:58 -0700

    chore(get): bump `statboard` version to 2.3

--- src/get.lua
+++ src/get.lua
@@ -173,7 +173,7 @@ local diskMap = {
         }
     },
     statboard = {
-        version = "2.2",
+        version = "2.3",
         name = "Stat Board",
         scripts = {
             scriptMap.get,
